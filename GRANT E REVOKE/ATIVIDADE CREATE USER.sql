3c981c3684657357f2cd00ce8a3cdbc79cf6664f
Fabrício Matos De Sousa <briciomattoss@gmail.com>
2025-02-21 21:47:41 -0300

    AJUSTES

--- GRANT E REVOKE/ATIVIDADE CREATE USER.sql
+++ GRANT E REVOKE/ATIVIDADE CREATE USER.sql
@@ -10,7 +10,7 @@ GRANT INSERT, UPDATE, SELECT ON PIZZARIA.* TO 'Luiz-Maia'@'%';
 
 -- Monitore as operações realizadas na tabela de auditoria au_pizza:
 USE pizzaria;
-SELECT * FROM au_pizza;
+SELECT * FROM pizza;
 
 -- Construa uma trigger para identificar os updates na tabela pizza e grave as antigas informações na tabela au_pizza:
 
@@ -22,3 +22,23 @@ BEGIN
 	INSERT INTO au_pizza(id_pizza, sabor, ingredientes, usario, data_hora, operacao)
     VALUES (OLD.id_pizza, OLD.sabor, OLD.ingredientes, user(), now(), 'update');
 END$$
+
+
+-- Construa uma trigger para identificar os deletes na tabela pizza e grave as antigas informações na tabela au_pizza:
+
+DELIMITER $$
+CREATE TRIGGER depois_delete_pizza
+AFTER DELETE ON pizza
+FOR EACH ROW
+BEGIN
+	INSERT INTO au_pizza(id_pizza, sabor, ingredientes, usario, data_hora, operacao)
+    VALUES (OLD.id_pizza, OLD.sabor, OLD.ingredientes, user(), now(), 'delete');
+END$$
+
+-- Conceda a permissão de delete para o usuário "luiz-maia"@"%":
+
+GRANT DELETE ON PIZZARIA.pizza TO 'Luiz-Maia'@'%';
+
+--  Conceda a permissão de select para o usuário "luiz-maia"@"%" na tabela au_pizza:
+
+GRANT SELECT ON PIZZARIA.au_pizza TO 'Luiz-Maia'@'%';
